a0a2f0095c5cc22d6a75be05302462b8627be085
anujtembhurne-ai <anuj.tembhurne@technokrate.com>
2024-09-26 14:41:33 +0530

    error fixing

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,5 @@
 const Joi = require("joi");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 const User = require("../models/admin");
 const { default: mongoose } = require("mongoose");
