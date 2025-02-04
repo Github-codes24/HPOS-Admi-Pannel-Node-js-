1d04f461e311d300d9b3bd0f0f13b44f6f0aae05
Aniket2421 <aniketnerkar123@gmail.com>
2025-02-04 18:28:16 +0530

    new

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -152,7 +152,7 @@ const getAllPatients = async (req, res) => {
     if (centerName) {
       queryFilter.centerName = centerName;
     }
-
+//add new code
     const allBreastCancerPatients = await BPatient.find(queryFilter);
     const allCervicalCancerPatients = await CPatient.find(queryFilter);
     const allSickleCellCancerPatients = await SPatient.find(queryFilter);
