7889c43327acd31129aa11e4b13d078f1b942e00
anujtembhurne-ai <anuj.tembhurne@technokrate.com>
2025-02-04 11:18:22 +0530

    error fixing

--- routes/breastPatientRoute.js
+++ routes/breastPatientRoute.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get("/getAllPatients", getAllPatients);
 router.get("/getAllPatientsCount", getAllPatientsCount);
 router.put("/updateBreastCancerPatient/:patientId", updateBreastCancerPatient);
-router.post("/deleteBreastCancerPatient/:patientId", deleteBreastCancerPatient);
+router.put("/deleteBreastCancerPatient/:patientId", deleteBreastCancerPatient);
 router.get("/getBreastCancerPatientById/:patientId", getBreastCancerPatientById);
 router.get("/getCenterCountsForBreastCancer", getCenterCountsForBreastCancer);
 router.get("/getAllPatientsForSubmittedForBreastCancer", getAllPatientsForSubmittedForBreastCancer);
